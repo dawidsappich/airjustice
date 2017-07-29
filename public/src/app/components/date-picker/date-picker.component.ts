8b12565162d1b0de944d3859d1cb349f91b99268
dawidsappich <dawidsappich@gmail.com>
2017-07-29 22:56:25 +0200

    date picker shows time

--- public/src/app/components/date-picker/date-picker.component.ts
+++ public/src/app/components/date-picker/date-picker.component.ts
@@ -1,28 +1,33 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, Input, AfterContentInit } from '@angular/core';
 import { Validators, FormControl } from '@angular/forms';
 
 declare var $: any;
 
 @Component({
-  selector: 'app-date-picker',
+  selector: 'date-picker',
   templateUrl: './date-picker.component.html',
   styleUrls: ['./date-picker.component.css']
 })
-export class DatePickerComponent implements OnInit {
+export class DatePickerComponent implements OnInit, AfterContentInit {
 
   flightDate: FormControl;
+  @Input() idSelector: string;
 
   constructor() { }
 
   ngOnInit() {
     this.flightDate = new FormControl('', Validators.required);
     // initialize the datepicker
+    // this.initializeDatePicker();
+  }
+
+  ngAfterContentInit() {
     this.initializeDatePicker();
   }
 
   initializeDatePicker() {
-    $('#datepicker').calendar({
-      type: 'date',
+    $('#' + this.idSelector).calendar({
+      type: 'time',
       firstDayOfWeek: 1,
       text: {
         days: ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'],
@@ -30,6 +35,14 @@ export class DatePickerComponent implements OnInit {
         monthsShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
         today: 'Heute',
         now: 'Jetzt'
+      },
+      formatter: {
+        time: (date, settings, forCalender) => {
+          const time = date.getHours() + 1;
+          const min = date.getMinutes();
+          const minFormatted = min.toString().length > 1 ? min : '0' + min;
+          return `${time}:${minFormatted}`;
+        }
       }
     });
   }
