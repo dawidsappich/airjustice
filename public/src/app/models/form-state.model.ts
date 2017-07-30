1ccdc32b924066d2fe8a182259c3857eda9befae
dawidsappich <dawidsappich@gmail.com>
2017-07-30 20:34:54 +0200

    handle response from REASON component

--- public/src/app/models/form-state.model.ts
+++ public/src/app/models/form-state.model.ts
@@ -1,3 +1,4 @@
+import { ReasonComponent } from './../components/reason/reason.component';
 import { NoclaimComponent } from './../components/noclaim/noclaim.component';
 import { ChoiceTrackerService } from './../services/choice-tracker.service';
 import { InitialFormComponent } from './../components/initial-form/initial-form.component';
@@ -24,6 +25,7 @@ export class FormState {
 		this.forms.set(FormStep.INITIAL, InitialFormComponent);
 		this.forms.set(FormStep.TIMING, FlightTimingComponent);
 		this.forms.set(FormStep.NOCALIM, NoclaimComponent);
+		this.forms.set(FormStep.REASON, ReasonComponent);
 	}
 
 	getCurrentStep(): FormStep {
@@ -31,6 +33,9 @@ export class FormState {
 	}
 
 	getnextForm(response: FormResponse) {
+		/**
+		 * EPK 1
+		 */
 		if (response.origin === FormStep.INITIAL) {
 			if (response.results && response.results.flightType === 'directFlight') {
 				this.currentStep = FormStep.TIMING;
@@ -41,11 +46,25 @@ export class FormState {
 			}
 		}
 
+		/**
+		 * EPK 1.1
+		 */
 		if (response.origin === FormStep.TIMING) {
 			if (this.userChoices.getChoice(FormStep.INITIAL).problemCase === 'delayedFlight' && response.results[response.results.length - 1].flightDelayedAmount >= 3) {
-				// TODO: Render form REASON
+				this.currentStep = FormStep.REASON;
+				return this.forms.get(this.getCurrentStep());
 			} else if (this.userChoices.getChoice(FormStep.INITIAL).problemCase === 'delayedFlight' && response.results[response.results.length - 1].flightDelayedAmount < 3) {
 				this.currentStep = FormStep.NOCALIM;
+				return this.forms.get(this.getCurrentStep());
+			}
+		}
+
+		/**
+		 * EPK 2
+		 */
+		if (response.origin === FormStep.REASON) {
+			if (response.results.reason === 'strikeReason' || response.results.reason === 'weatherReason') {
+				this.currentStep = FormStep.NOCALIM
 				return this.forms.get(this.getCurrentStep());
 			}
 		}
